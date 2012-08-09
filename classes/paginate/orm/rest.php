0f8c42e4168aa4fa8a3ad8f8af0010044f9eeb60
Micheal Morgan <micheal@morgan.ly>
2012-08-08 23:16:05 -0400

    Added support for ORM-REST module.

---
+++ classes/paginate/orm/rest.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Paginate ORM REST driver
+ * 
+ * @package		Paginate
+ * @author		Micheal Morgan <micheal@morgan.ly>
+ * @copyright	(c) 2011-2012 Micheal Morgan
+ * @license		MIT
+ */
+class Paginate_ORM_REST extends Kohana_Paginate_ORM_REST {}
